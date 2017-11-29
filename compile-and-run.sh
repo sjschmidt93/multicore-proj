3e17f760c316201838f8bb3a9a456100a989e5c2
Steven <sjschmidt93@gmail.com>
2017-11-29 19:37:19 +0900

    avg pixel for directory of bmps

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-g++ proj.cpp EasyBMP_1.06/EasyBMP.cpp -o proj
-./proj uncompressed_24.bmp
+g++ -std=c++11 proj.cpp EasyBMP_1.06/EasyBMP.cpp -o proj -lboost_system -lboost_filesystem
+./proj test_dir
